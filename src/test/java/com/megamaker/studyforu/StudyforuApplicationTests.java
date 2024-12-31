a3ade575e0a6c8b673b170f4c5da8d07222b5587
ness727 <gnsdl727@naver.com>
2024-12-31 12:01:18 +0900

    프로젝트 시작

---
+++ src/test/java/com/megamaker/studyforu/StudyforuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megamaker.studyforu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyforuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
