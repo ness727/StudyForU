681a76a4f4f194058cfeb8df1504cd3d33a1dce5
ness727 <gnsdl727@naver.com>
2025-01-01 21:14:14 +0900

    게시글 상태 VO 및 SearchCond 추가

---
+++ src/main/java/com/megamaker/studyforu/post/domain/dto/PostSearchCond.java
@@ -0,0 +1,12 @@
+package com.megamaker.studyforu.post.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PostSearchCond {
+    private String title;
+    private Integer minPrice;
+    private Integer maxPrice;
+}
---
+++ src/main/java/com/megamaker/studyforu/post/domain/vo/Status.java
@@ -0,0 +1,10 @@
+package com.megamaker.studyforu.post.domain.vo;
+
+/**
+ * PUBLIC: 전체 공개
+ * PRIVATE: 비밀글
+ * SECRET: 포인트로 구매한 사람만 열람 가능
+ */
+public enum Status {
+    PUBLIC, PRIVATE, SECRET
+}
