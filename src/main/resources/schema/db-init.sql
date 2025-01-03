d781b23fcd5cfbaa137022ea4379fb0b47a07370
ness727 <gnsdl727@naver.com>
2025-01-03 12:44:19 +0900

    UserInfo VO 추가에 따른 ddl 변경

--- src/main/resources/schema/db-init.sql
+++ src/main/resources/schema/db-init.sql
@@ -7,5 +7,25 @@ create table users(
     provider_id varchar(255),
     provider_nickname varchar(255),
     username varchar(100),
-    role varchar(20)
+    role varchar(20),
+    point integer,
+    created_at datetime,
+    updated_at datetime
 );
+
+create table posts(
+    id bigint primary key auto_increment,
+    user_id bigint,
+    writer_name varchar(100),
+    category_id integer,
+    title varchar(255),
+    body text,
+    likes integer,
+    status varchar(30),
+    created_at datetime,
+    updated_at datetime
+);
+
+create table category(
+
+)
