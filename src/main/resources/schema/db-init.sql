9b4032054c75340fd24e2390ac4952cf72822335
ness727 <gnsdl727@naver.com>
2025-01-04 21:00:29 +0900

    카테고리 스키마 변경

--- src/main/resources/schema/db-init.sql
+++ src/main/resources/schema/db-init.sql
@@ -3,29 +3,36 @@ create database study_for_u;
 use study_for_u;
 create table users(
     id bigint primary key auto_increment,
-    provider varchar(30),
-    provider_id varchar(255),
+    provider varchar(30) not null,
+    provider_id varchar(255) not null,
     provider_nickname varchar(255),
     username varchar(100),
-    role varchar(20),
-    point integer,
-    created_at datetime,
-    updated_at datetime
+    role varchar(20) not null,
+    point integer default 0 not null,
+    created_at datetime not null,
+    updated_at datetime not null
 );
 
 create table posts(
     id bigint primary key auto_increment,
-    user_id bigint,
+    user_id bigint not null,
     writer_name varchar(100),
     category_id integer,
     title varchar(255),
     body text,
     likes integer,
     status varchar(30),
-    created_at datetime,
-    updated_at datetime
+    created_at datetime not null,
+    updated_at datetime not null
 );
 
-create table category(
+create table categories(
+    id bigint primary key auto_increment,
+    parent_id bigint,
+    level integer not null,
+    name varchar(100),
+    created_at datetime not null,
+    updated_at datetime not null,
 
+    foreign key (parent_id) references categories(id)
 )
